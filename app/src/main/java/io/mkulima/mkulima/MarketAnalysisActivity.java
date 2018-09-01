c1d1960a741640b970b2e82f015319ec7c920d1a
Walter Sanchez <walteranyika@gmail.com>
2018-09-01 10:04:37 +0300

    Added Analysis Graphs

--- app/src/main/java/io/mkulima/mkulima/MarketAnalysisActivity.java
+++ app/src/main/java/io/mkulima/mkulima/MarketAnalysisActivity.java
@@ -3,11 +3,27 @@ package io.mkulima.mkulima;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class MarketAnalysisActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market_analysis);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1200),
+                new DataPoint(1, 1450),
+                new DataPoint(2, 1143),
+                new DataPoint(3, 1600),
+                new DataPoint(4, 1870),
+                new DataPoint(5, 1900),
+                new DataPoint(6, 1650),
+                new DataPoint(7, 100)
+        });
+        graph.addSeries(series);
     }
 }
--- app/src/main/java/io/mkulima/mkulima/ValueAdditionActivity.java
+++ app/src/main/java/io/mkulima/mkulima/ValueAdditionActivity.java
@@ -2,6 +2,7 @@ package io.mkulima.mkulima;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class ValueAdditionActivity extends AppCompatActivity {
 
@@ -9,5 +10,7 @@ public class ValueAdditionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_value_addition);
+        WebView webView=findViewById(R.id.webView);
+        webView.loadUrl("file:///android_asset/onion_value.html");
     }
 }
